bb5916dd3949eee9802d979d93572c47281d00bb
Lucas Monteiro <lucas@ecoportal.co.nz>
2023-02-03 18:40:55 -0300

    Better interface

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,22 @@
-use std::cmp;
 use serde::{Deserialize, Serialize};
+use termion::input::TermRead;
+use termion::raw::RawTerminal;
+use::termion::event::Key;
 use std::fs;
+use std::env;
+use termion::raw::IntoRawMode;
+use std::io::{Write, stdout, Stdout, stdin};
 
 const WIDTH: usize = 80;
 const HEIGHT: usize = 24;
+
+const COLUMNS_AMOUNT: usize = 3;
+const COLUMN_WIDTH: usize = WIDTH/COLUMNS_AMOUNT;
+
+enum Mode {
+    Normal,
+    Add
+}
 
 type Buffer = [[char; WIDTH]; HEIGHT];
 
@@ -19,20 +32,45 @@ struct TextObject {
 }
 
 #[derive(Serialize, Deserialize)]
-struct JsonData {
-    todo: Vec<String>,
-    doing: Vec<String>,
-    done: Vec<String>,
+struct Task {
+    text: String,
+    position: usize
 }
 
-fn draw_buffer(buffer: Buffer) {
+struct Column<'a> {
+    title: String,
+    tasks: &'a Vec<Task>
+}
+
+#[derive(Serialize, Deserialize)]
+struct JsonData {
+    todo: Vec<Task>,
+    doing: Vec<Task>,
+    done: Vec<Task>,
+}
+
+fn draw_buffer(buffer: Buffer, stdout: &mut RawTerminal<Stdout>) {
     for line in buffer.iter() {
-        println!("{}", String::from_iter(line));
+        let mut output = String::from_iter(line);
+        output.push_str("\r\n");
+        write!(stdout, "{output}").unwrap();
     }
 }
 
+fn draw_options(stdout: &mut RawTerminal<Stdout>) {
+    write!(stdout, "\r\n").unwrap();
+    write!(stdout, "Commands:\r\n").unwrap();
+    write!(stdout, "\r\n").unwrap();
+    write!(stdout, "    h/l -> Cycle through selected columns\r\n").unwrap();
+    write!(stdout, "    j/k -> Cycle through selected tasks\r\n").unwrap();
+    write!(stdout, "    a -> Add task to selected column\r\n").unwrap();
+    write!(stdout, "    H/L -> Move selected task to next column\r\n").unwrap();
+    write!(stdout, "    D -> Remove selected task\r\n").unwrap();
+    write!(stdout, "    Q -> Save and quit\r\n").unwrap();
+}
+
 fn clear_screen() {
-    print!("\x2B[2J\x1B[1;1H");
+    print!("\x1B[2J\x1B[1;1H");
 }
 
 fn draw_text_object(buffer: &mut Buffer, text_object: TextObject) {
@@ -67,46 +105,139 @@ fn draw_text_at_column(buffer: &mut Buffer, column_index: usize, text: &String) 
     draw_text_object(buffer, text_object);
 }
 
+fn draw_column(buffer: &mut Buffer, column_index: usize, column: &Column) {
+    draw_column_title(buffer, column_index, &column.title);
+    for task in column.tasks {
+        draw_text_at_column(buffer, column_index, &task.text);
+    }
+}
+
+fn draw_frame(buffer: &mut Buffer, selected_column: usize) {
+    // Corners
+    buffer[0][0] = match selected_column { 0 => '\u{250F}', _ => '\u{250C}'};
+    buffer[0][WIDTH-1] = match selected_column { 2 => '\u{2513}', _ => '\u{2510}'};
+    buffer[HEIGHT-1][0] = match selected_column { 0 => '\u{2517}', _ =>'\u{2514}'};
+    buffer[HEIGHT-1][WIDTH-1] = match selected_column { 2 => '\u{251B}', _ =>'\u{2518}'};
+
+    // Vertical
+    for i in 1..HEIGHT-1 {
+        buffer[i][0] = match selected_column { 0 => '\u{2503}', _ => '\u{2502}'};
+        buffer[i][WIDTH-1] = match selected_column { 2 => '\u{2503}', _ => '\u{2502}' };
+        buffer[i][WIDTH/3] = match selected_column { 2 => '\u{2502}', _ => '\u{2503}' };
+        buffer[i][2*WIDTH/3] = match selected_column { 0 => '\u{2502}', _ => '\u{2503}' };
+    }
+
+    // Horizontal
+    for i in 1..WIDTH-1 {
+        let in_selected = i > selected_column*COLUMN_WIDTH && i < selected_column*COLUMN_WIDTH + COLUMN_WIDTH;
+        let normal_char: char = match in_selected { false => '\u{2500}', true => '\u{2501}' };
+        let bottom_split: char = '\u{2534}';
+        let top_split: char = '\u{252C}';
+
+        if i == WIDTH/3 || i == 2*WIDTH/3 {
+            buffer[0][i] = top_split;
+            buffer[HEIGHT-1][i] = bottom_split;
+            continue;
+        }
+        buffer[0][i] = normal_char;
+        buffer[HEIGHT-1][i] = normal_char;
+    }
+}
+
+fn create_db() {
+    fs::create_dir_all("data").expect("Cannot create data dir");
+    let empty_data: JsonData = JsonData { todo: vec![], doing: vec![], done: vec![] };
+    let empty_data = serde_json::to_string(&empty_data).expect("Invalid json");
+    fs::write("data/data.json", empty_data).expect("Cannot write to data/data.json");
+}
+
 fn main() {
+    let args: Vec<String> = env::args().collect();
+    // Migrate database
+    if args.len() == 2 && args[1] == "migrate" {
+        create_db();
+        return
+    }
+
+    let mut stdout = stdout().into_raw_mode().unwrap();
 
     let mut buffer: Buffer = [[' '; WIDTH]; HEIGHT];
 
-    
-    buffer[0][0] = '*';
-    buffer[0][WIDTH-1] = '*';
-    buffer[HEIGHT-1][0] = '*';
-    buffer[HEIGHT-1][WIDTH-1] = '*';
+    let mut columns: [Column; 3] = [
+        Column {
+            title: String::from("TODO"),
+            tasks: &vec![]
+        },
+        Column {
+            title: String::from("DOING"),
+            tasks: &vec![]
+        },
+        Column {
+            title: String::from("DONE"),
+            tasks: &vec![]
+        }
+    ];
 
-    for i in 1..HEIGHT-1 {
-        buffer[i][0] = '|';
-        buffer[i][WIDTH-1] = '|';
-        buffer[i][WIDTH/3] = '|';
-        buffer[i][2*WIDTH/3] = '|';
+
+    for i in 0..columns.len() {
+        draw_column(&mut buffer, i, &columns[i]);
     }
-    for i in 1..WIDTH-1 {
-        buffer[0][i] = '-';
-        buffer[HEIGHT-1][i] = '-';
-    }
 
-    draw_column_title(&mut buffer, 0, &String::from("TODO"));
-    draw_column_title(&mut buffer, 1, &String::from("DOING"));
-    draw_column_title(&mut buffer, 2, &String::from("DONE"));
-
-    let data = fs::read_to_string("/home/lucas/Dev/todo-rust/src/data.json").expect("Couldnt open file");
+    let data = fs::read_to_string("data/data.json").expect("Couldnt open file");
     let data: JsonData = serde_json::from_str(data.as_str()).expect("Couldnt parse json");
-    let iterations = cmp::max(cmp::max(data.todo.len(), data.doing.len()), data.done.len());
-    for i in 0..iterations { 
-        if i < data.todo.len() {
-            draw_text_at_column(&mut buffer, 0, &data.todo[i]);
-        }
-        if i < data.doing.len() {
-            draw_text_at_column(&mut buffer, 1, &data.doing[i]);
-        }
-        if i < data.done.len() {
-            draw_text_at_column(&mut buffer, 2, &data.done[i]);
+
+    for i in 0..columns.len() {
+        columns[i].tasks = match i {
+            0 => &data.todo,
+            1 => &data.doing,
+            2 => &data.done,
+            _ => panic!("Too many columns")
+        };
+
+        draw_column(&mut buffer, i, &columns[i]);
+    }
+
+    let mut selected_column: usize = 0;
+    let mut mode: Mode = Mode::Normal;
+    loop {
+        let stdin = stdin();
+        clear_screen();
+        draw_frame(&mut buffer, selected_column);
+        draw_buffer(buffer, &mut stdout);
+        draw_options(&mut stdout);
+        write!(stdout, "\r\n    Selected column: {}\r\n", match selected_column {
+            0 => "TODO",
+            1 => "DOING",
+            2 => "DONE",
+            _ => "Unknown column"
+        }).unwrap();
+        for character in stdin.keys() {
+            match mode {
+                Mode::Normal => match character.unwrap() {
+                    Key::Char('Q') => {
+                        // Save
+                        return
+                    }
+                    Key::Char('h') => {
+                        if selected_column == 0 {
+                            selected_column = 2;
+                            break;
+                        }
+                        selected_column -= 1;
+                        break;
+                    }
+                    Key::Char('l') => {
+                        selected_column = (selected_column + 1) % 3;
+                        break;
+                    }
+                    Key::Char('a') => {
+                        mode = Mode::Add;
+                        break;
+                    }
+                    _ => {}
+                },
+                Mode::Add => {}
+            }
         }
     }
-    clear_screen();
-    draw_buffer(buffer);
-    
 }
